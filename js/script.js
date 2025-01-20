78da81dfaba0e47a34180e318fc2422a1244e2e1
MDJAmin <mohammadamindf@gmail.com>
2025-01-20 11:47:10 +0330

    Functionality for slider

--- js/script.js
+++ js/script.js
@@ -5,3 +5,23 @@ const navLinks = document.querySelector(".nav-links");
 hamburger.addEventListener("click", () => {
   navLinks.classList.toggle("active");
 });
+
+
+
+const swiper = new Swiper('.mySwiper', {
+  loop: true,          
+  spaceBetween: 20,    
+  slidesPerView: 1,    
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev'
+  },
+  autoplay: {
+    delay: 3000,       
+    disableOnInteraction: false, 
+  },
+  pagination: {
+    el: '.swiper-pagination',   
+    clickable: true,            
+  },
+});
